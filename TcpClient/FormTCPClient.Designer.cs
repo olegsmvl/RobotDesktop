818369bc947d449d3bc503b512ba53d14eaf2c9a
olegsmvl <33567178+olegsmvl@users.noreply.github.com>
2019-12-14 12:47:48 +0300

    add tcpClient

---
+++ TcpClient/FormTCPClient.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TcpClientProject
+{
+    partial class FormTCPClient
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbData
+            // 
+            this.tbData.Location = new System.Drawing.Point(149, 21);
+            this.tbData.Multiline = true;
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(685, 629);
+            this.tbData.TabIndex = 0;
+            // 
+            // FormTCPClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 726);
+            this.Controls.Add(this.tbData);
+            this.Name = "FormTCPClient";
+            this.Text = "TcpClient";
+            this.Shown += new System.EventHandler(this.FormTCPClient_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbData;
+    }
+}
+
---
+++ TcpClient/FormTCPClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpClientProject
+{
+    public partial class FormTCPClient : Form
+    {
+        private string Message = string.Empty;
+        public FormTCPClient()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void FormTCPClient_Shown(object sender, EventArgs e)
+        {
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect("192.168.1.94", 5051);
+            var stream = tcpClient.GetStream();
+            byte[] data = new byte[256];
+            int bytes = stream.Read(data, 0, data.Length);
+            Message = Encoding.UTF8.GetString(data, 0, bytes);
+
+            tbData.Text += Message;
+        }
+    }
+}
